e4e477ca16aa5e1246ca9e8e4c715ee49be219a5
gamemann <gamemann@gflclan.com>
2016-06-28 19:46:19 -0400

    Added Support For SourceMod & MetaMod Branches
    Also cleaned up the code...

--- batch/updatesdks.bat
+++ batch/updatesdks.bat
@@ -1,15 +1,13 @@
 :: File Name: 		update.bat
 :: File Author: 	Roy (Christian Deacon)
 :: File Date:		6-22-16
+:: File Version: 	1.1 (6-26-16)
 
 @echo off 
 
 :: Variables and such.
 SET MAINPATH=E:\Coding\Dependencies\sdks
-SET PREFIX=hl2sdk_
 SET VERIFY=1
-SET SOURCEMODFOLDER=sourcemod-sdk
-SET METAMODFOLDER=metamod-sdk
 
 :: HL2SDKs
 SET SDKS[0]=csgo
@@ -35,18 +33,35 @@ SET SDKS[19]=dota
 SET SDKS[20]=sdk2013
 SET SDKS[21]=portal2
 
+SET SDKSPREFIX=hl2sdk-
+
+:: SourceMod Branches. Note: I only did this for the ones I use.
+SET SM[0]=master
+SET SM[1]=1.3-dev
+SET SM[2]=1.4-dev
+SET SM[3]=1.7-dev
+SET SM[4]=1.5-dev
+
+SET SMPREFIX=sourcemod-
+
+:: MetaMod Branches. Note: I only did this for the ones I use.
+SET MM[0]=master
+SET MM[1]=1.7-dev
+SET MM[2]=1.8-dev
+SET MM[3]=1.9-dev
+
+SET MMPREFIX=metamod-
+
 :: Update the HL2SDKs.
 echo Updating HL2SDKs...
 echo.
 timeout /t 1 >nul 2>&1
 
 for /F "tokens=2 delims==" %%s in ('set SDKS[') do (
-	cd %MAINPATH%
-	
 	:: Check if the folder exist or not.
-	if exist %MAINPATH%\%PREFIX%%%s (
+	if exist %MAINPATH%\%SDKSPREFIX%%%s (
 		:: Verify and Git Pull.
-		cd %MAINPATH%\%PREFIX%%%s
+		cd %MAINPATH%\%SDKSPREFIX%%%s
 		
 		if %VERIFY%==1 (
 			git checkout .
@@ -55,12 +70,12 @@ for /F "tokens=2 delims==" %%s in ('set SDKS[') do (
 		git pull
 	) else (
 		:: Make directory + git clone.
-		mkdir %MAINPATH%\%PREFIX%%%s >nul 2>&1
-		cd %MAINPATH%\%PREFIX%%%s
+		mkdir %MAINPATH%\%SDKSPREFIX%%%s >nul 2>&1
+		cd %MAINPATH%\%SDKSPREFIX%%%s
 		git clone -b %%s https://github.com/alliedmodders/hl2sdk.git .
 	)
 	
-	echo %PREFIX%%%s Updated...
+	echo %SDKSPREFIX%%%s Updated...
 	echo.
 
 	timeout /t 1 >nul 2>&1
@@ -70,59 +85,68 @@ timeout /t 1 >nul 2>&1
 echo HL2SDKs updated...
 
 :: Update the SourceMod SDK.
-echo Updating SourceMod SDK...
+echo Updating SourceMod SDKs...
 echo.
 timeout /t 1 >nul 2>&1
 
-cd %MAINPATH%
-
-:: Check if the folder exist or not.
-if exist %MAINPATH%\%SOURCEMODFOLDER% (
-	:: Verify and Git Pull.
-	cd %MAINPATH%\%SOURCEMODFOLDER%
-	
-	if %VERIFY%==1 (
-		git checkout .
+for /F "tokens=2 delims==" %%s in ('set SM[') do (
+	:: Check if the folder exist or not.
+	if exist %MAINPATH%\%SMPREFIX%%%s (
+		:: Verify and Git Pull.
+		cd %MAINPATH%\%SMPREFIX%%%s
+		
+		if %VERIFY%==1 (
+			git checkout .
+		)
+		
+		git pull
+	) else (
+		:: Make directory + git clone.
+		mkdir %MAINPATH%\%SMPREFIX%%%s >nul 2>&1
+		cd %MAINPATH%\%SMPREFIX%%%s
+		git clone -b %%s --recursive https://github.com/alliedmodders/sourcemod.git .
 	)
 	
-	git pull
-) else (
-	:: Make directory + git clone.
-	mkdir %MAINPATH%\%SOURCEMODFOLDER% >nul 2>&1
-	cd %MAINPATH%\%SOURCEMODFOLDER%
-	git clone -b master --recursive https://github.com/alliedmodders/sourcemod.git .
+	echo %SMPREFIX%%%s Updated...
+	echo.
+
+	timeout /t 1 >nul 2>&1
 )
 
-echo SourceMod SDK Updated...
+echo SourceMod SDKs Updated...
 echo.
-
 timeout /t 1 >nul 2>&1
 
 :: Update the MetaMod SDK.
-echo Updating MetaMod SDK...
+echo Updating MetaMod SDKs...
 echo.
 timeout /t 1 >nul 2>&1
 
-cd %MAINPATH%
-
-:: Check if the folder exist or not.
-if exist %MAINPATH%\%METAMODFOLDER% (
-	:: Verify and Git Pull.
-	cd %MAINPATH%\%METAMODFOLDER%
-	
-	if %VERIFY%==1 (
-		git checkout .
+for /F "tokens=2 delims==" %%s in ('set MM[') do (
+	:: Check if the folder exist or not.
+	if exist %MAINPATH%\%MMPREFIX%%%s (
+		:: Verify and Git Pull.
+		cd %MAINPATH%\%MMPREFIX%%%s
+		
+		if %VERIFY%==1 (
+			git checkout .
+		)
+		
+		git pull
+	) else (
+		:: Make directory + git clone.
+		mkdir %MAINPATH%\%MMPREFIX%%%s >nul 2>&1
+		cd %MAINPATH%\%MMPREFIX%%%s
+		git clone -b %%s --recursive https://github.com/alliedmodders/metamod-source.git .
 	)
 	
-	git pull
-) else (
-	:: Make directory + git clone.
-	mkdir %MAINPATH%\%METAMODFOLDER% >nul 2>&1
-	cd %MAINPATH%\%METAMODFOLDER%
-	git clone -b master --recursive https://github.com/alliedmodders/metamod-source.git .
+	echo %MMPREFIX%%%s Updated...
+	echo.
+
+	timeout /t 1 >nul 2>&1
 )
 
-echo MetaMod SDK Updated...
+echo MetaMod SDKs Updated...
 echo.
 
 timeout /t 1 >nul 2>&1
