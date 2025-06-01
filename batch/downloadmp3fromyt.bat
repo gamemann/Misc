30b3944a9f525fefd9812c7da4cd8cb2cdd8ad0a
Christian Deacon <christianmdeacon@gmail.com>
2025-06-01 12:02:41 -0400

    Improve script.

--- batch/downloadmp3fromyt.bat
+++ batch/downloadmp3fromyt.bat
@@ -1,17 +1,17 @@
 @echo off
 
-IF "%1"=="" (
+if "%1"=="" (
     set /p "url=URL: "
-) ELSE (
+) else (
     set url=%1=%2
 )
-
-echo Downloading %url%
 
 if "%url%"=="" (
     echo No URL specified
     exit /b 1
 )
+
+echo Downloading %url%
 
 yt-dlp --ffmpeg-location .\bin -x --audio-format mp3 --audio-quality 0 -P ".\downloads" %url%
 
